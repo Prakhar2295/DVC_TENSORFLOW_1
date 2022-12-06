56053a12fe02a6524c4e4a75f7f69f0ab3510ba8
Prakhar2295 <prakhar.kumar5@gmail.com>
2022-12-06 21:32:28 +0530

    load & save not working

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -2,43 +2,33 @@ from src.utils.all_utils import read_yaml,create_directory
 import argparse
 import pandas as pd
 import os
+import shutil
+
+
+def copy_file(source_download_dir, local_data_dir):
+    list_of_files = os.listdir(source_download_dir)
+    N = len(list_of_files)
+    for file in list_of_files:
+        src = os.path.join(source_download_dir, file)
+        dest = os.path.join(local_data_dir, file)
+        shutil.copy(src, dest)
+    print(source_download_dir)
 
 
 def get_data(config_path):
     config = read_yaml(config_path)
 
+    source_download_dirs = config["source_download_dirs"]
+    local_data_dirs = config["local_data_dirs"]
 
-    remote_data_path = config["data_source"]
-    df = pd.read_csv(remote_data_path, sep= ";")
-
-    # save dataset in the local directory
-    # create path to directory: artifacts/raw_local_dir/data.csv
-    artifacts_dir = config['artifacts']['artifacts_dir']
-    raw_local_dir = config["artifacts"]['raw_local_dir']
-    raw_local_file = config['artifacts']['raw_local_file']
-
-    raw_local_dir_path = os.path.join(artifacts_dir,raw_local_dir)
-
-    create_directory(dirs = [raw_local_dir_path])
-
-    
-
-    raw_local_file_path = os.path.join(raw_local_dir_path,raw_local_file)
-    print(raw_local_file_path)
-
-    df.to_csv(raw_local_file_path,sep=',',index = False)
+    for source_download_dir, local_data_dir in zip(source_download_dirs, local_data_dirs):
+        create_directory([local_data_dir])
+        copy_file(source_download_dir, local_data_dir)
 
 
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
 
-    
+    args.add_argument("--config", "-c", default="config/config.yaml")
 
-
-
-if __name__ == "__main__":
-     args = argparse.ArgumentParser()
-
-     args.add_argument("--config","-c",default = "config/config.yaml")
-
-     parsed_args = args.parse_args()
-
-     get_data(config_path=parsed_args.config)
+    parsed_args = args.parse_args()
