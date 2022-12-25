6159b43735aa923f226ae690e2cb586e7931d216
Prakhar2295 <prakhar.kumar5@gmail.com>
2022-12-26 00:26:50 +0530

    stage 1 completed succesfully & stage 2 started

--- src/stage_02_prepare_base_model.py
+++ src/stage_02_prepare_base_model.py
@@ -1,5 +1,5 @@
 from src.utils.all_utils import read_yaml, create_directory
-src.utils.models import get_vgg_16_model
+from src.utils.models import get_VGG_16_model
 import argparse
 import pandas as pd
 import os
@@ -9,11 +9,9 @@ import logging
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
 log_dir = "logs"
-os.mkdir(log_dir)
-logging.basicConfig(filename= os.path.join(log_dir, "running_logs.log"), level=logging.INFO,
-format= logging_str,filemode="a")
-
-
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=logging.INFO, format=logging_str,
+                    filemode="a")
 
 
 
@@ -22,16 +20,23 @@ def prepare_base_model(config_path,params_path):
     params = read_yaml(params_path)
 
     artifacts = config["artifacts"]
-    artifacts_dir = artifacts[ARTIFACTS_DIR]
+    artifacts_dir = artifacts["ARTIFACTS_DIR"]
 
     base_model_dir = artifacts["BASE_MODEl_DIR"]
-    base_model_name = artifacts["BASE_MODEL_NAME"]
+    base_model_name = artifacts["BASE_MODEl_NAME"]
 
     base_model_dir_path = os.path.join(artifacts_dir,base_model_dir)
+
     create_directory([base_model_dir_path])
 
     base_model_path = os.path.join(base_model_dir_path,base_model_name)
-    model = get_vgg_16_model(input_shape=params["IMAGE_SIZE"],model_path= base)
+
+    model = get_VGG_16_model(input_shape=params["IMAGE_SIZE"],model_path = base_model_path)
+
+    
+
+
+
 
 
     
@@ -41,15 +46,14 @@ if __name__ == '__main__':
     args = argparse.ArgumentParser()
 
     args.add_argument("--config", "-c", default="config/config.yaml")
-    args.add_argument("--params", "-p", default=params.yaml")
+    args.add_argument("--params", "-c", default="params.yaml")
 
     parsed_args = args.parse_args()
-    
-    try:
-        logging.info(">>>> stage two started")
-        prepare_base_model(config_path= parsed_args.config,params_path= parsed_args.params)
-        logging.info("stage two completed! base model is created  >>>>>")
 
+    try:
+        logging.info(">>>>> stage two started")
+        prepare_base_model(config_path=parsed_args.config,params_path=parsed_args.params)
+        logging.info("stage two completed! base model is created >>>>>\n")
     except Exception as e:
         logging.exception(e)
         raise e
